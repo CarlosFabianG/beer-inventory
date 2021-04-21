970fd40b70143a907b2524d8de85481b618f69d7
OmarMorales71 <66653667+OmarMorales71@users.noreply.github.com>
2021-04-20 19:26:35 -0500

    Add CROS and bug fixed (#22)
    * Add cors to beer controller
    
    * Fix cast to from List<Beer> to List<BeerDto>
    
    Co-authored-by: Omar Morales <omar.ortega@oracle.com>

--- src/main/java/com/academy/beerinventory/web/controller/BeerController.java
+++ src/main/java/com/academy/beerinventory/web/controller/BeerController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/beer")
 public class BeerController {
 
